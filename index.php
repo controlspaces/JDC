e81134ca43e36e314b5d808e28a0f88d640904d4
camilap96 <melizacamila_96@hotmail.com>
2017-06-06 12:23:46 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+	</head>
+    <body>
+        <div id="mainContent">
+            <?php
+                require_once("header.php");
+            ?>
+            
+        </div>
+    </body>
+</html>
