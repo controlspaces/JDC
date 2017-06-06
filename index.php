5cb46b71a331c5ea659493d6e31cbb2cdfb1fbfd
camilap96 <melizacamila_96@hotmail.com>
2017-06-06 15:33:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 	<head>
 		<meta charset="UTF-8">
 		<title>Document</title>
@@ -8,8 +7,9 @@
         <div id="mainContent">
             <?php
                 require_once("header.php");
+                require_once("login.php");
+                require_once("footer.php");
             ?>
-            
         </div>
     </body>
 </html>
