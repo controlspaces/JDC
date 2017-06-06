5af25d3d5db243a35af24e019e5d5c1707760afb
camilap96 <melizacamila_96@hotmail.com>
2017-06-06 15:34:36 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,9 @@
+<div id="content">
+    <div class="content_login" id="usuario">
+        Usuario: <input type="text" class="content_input" name="usuario"/>
+    </div>
+    <div class="content_login" id="contrasena">
+        Contraseña: <input type="password" class="content_input" name="contrasena"/>
+    </div>
+    <div class="botones" id="ingreso_btn">Ingresar</div>
+</div>
