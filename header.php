ffef39eeea2edf69bbf21cc25b1fb92981f3338b
camilap96 <melizacamila_96@hotmail.com>
2017-06-06 12:23:03 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+	</head>
+    <body>
+        <div id="header">
+            <img src="img/logo-jdc-vertica.png" id="logo-jdc"/>
+            <div id="titulo">ControlSpaces JDC</div>
+        </div>
+        
+    </body>
+</html>
