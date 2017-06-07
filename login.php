93ba7cf399402d170d3c76325742c91bd34509c2
camilap96 <melizacamila_96@hotmail.com>
2017-06-06 20:42:16 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,5 +5,8 @@
     <div class="content_login" id="contrasena">
         Contraseña: <input type="password" class="content_input" name="contrasena"/>
     </div>
-    <div class="botones" id="ingreso_btn">Ingresar</div>
+    <a href="modulos.php">
+        <div class="botones" id="ingreso_btn">Ingresar</div>
+    </a>
+    
 </div>
