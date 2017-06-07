22855be237bdde40fd0db403b89b0e0df8f4480c
camilap96 <melizacamila_96@hotmail.com>
2017-06-06 20:40:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Document</title>
+        <script type="text/javascript" src="js/index.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link rel="stylesheet" type="text/css" href="css/fonts/style.css">
 	</head>
     <body>
         <div id="mainContent">
