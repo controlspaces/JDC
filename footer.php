b9197f75c67a112a56dba020e20cd4e073760dc9
camilap96 <melizacamila_96@hotmail.com>
2017-06-08 15:08:17 -0500

    Create footer.php

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
     <div class="footer" id="universidad">Fundación Universitaria Juan de Castellanos</div>
     <div class="footer" id="nombres">Camila Peña | Sandra Chaparro | Jhon Rodriguez</div>
     <div class="footer" id="programa">Ingeniería de Sistemas</div>
-    <div class="footer" id="ano">2017</div>
+    <div class="footer" id="year">2017</div>
 </div>
