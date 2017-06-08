4b9a7d77dcced6bd2e9f46513154aaf739c2d31c
camilap96 <melizacamila_96@hotmail.com>
2017-06-08 17:53:00 -0500

    Create index.php

--- index.php
+++ index.php
@@ -1,18 +1,32 @@
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<title>Document</title>
-        <script type="text/javascript" src="js/index.js"></script>
-        <link rel="stylesheet" type="text/css" href="css/index.css">
-        <link rel="stylesheet" type="text/css" href="css/fonts/style.css">
+		<meta charset="utf-8"/>
+		<meta name="description" content="asignacion de espacios, prestamos, juan de castellanos"/>
+		<title>ControlSpaces JDC</title>
+		<script type="text/javascript" src="js/jquery-3.2.0.min.js"></script>
+		<script type="text/javascript" src="js/index.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
 	</head>
+    
     <body>
         <div id="mainContent">
+            
             <?php
                 require_once("header.php");
-                require_once("login.php");
+            ?>
+            
+		<div id="content">
+			<div id="content_list">
+				<div class="input" id="usuario">Usuario: <input class="texto" type="text" name="usuario" id="usuario-input"></div>
+				<div class="input" id="password">Contraseña: <input class="texto" type="password" name="password" id="password-input"></div>
+				<div class="botones" id="ingresar">Ingresar</div>
+			</div>
+		</div>
+            
+           <?php
                 require_once("footer.php");
             ?>
+            
         </div>
     </body>
 </html>
